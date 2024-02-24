549f6b1886fb8b7c2e02d8c92a8ced68a97ef8e3
krishbedi17 <106929134+krishbedi17@users.noreply.github.com>
2024-02-23 17:11:04 -0800

    Update printerThread.c

--- printerThread.c
+++ printerThread.c
@@ -7,34 +7,33 @@
 #define MAX_LEN 1024
 
 pthread_t thread;
-List* list;
+List *list;
 
+void *printerThread(void *unused)
+{
 
-void * printerThread(void * unused){
-
-    while (1) {
-        waitForRecieverSignal();
+    while (1)
+    {
         pthread_mutex_lock(&recieverListMutex);
         {
-            pthread_cond_wait(&recieverListToMonitorCond, &recieverListMutex);
+            //while (!list) // while list is empty work in kardia jaaye somehow
+            {
+                pthread_cond_wait(&recieverListToMonitorCond, &recieverListMutex);
+            }
             List_first(list);
-            char* x = List_remove(list);
+            char *x = List_remove(list);
             printf("Message Recieved: \n\n'%s'\n", x);
         }
         pthread_mutex_unlock(&recieverListMutex);
     }
-} 
+}
 
-void waitForRecieverSignal() {
-   while (1) {
-        pthread_cond_wait(&recieverListToMonitorCond, &recieverListMutex);
-        break;
-   }
-}
-void Printer_init(List* recieverList) {
+void Printer_init(List *recieverList)
+{
     list = recieverList;
-    pthread_create(&thread,NULL,printerThread,NULL);
+    pthread_create(&thread, NULL, printerThread, NULL);
 }
-void Printer_shutdown(){
-    pthread_join(thread,NULL);
+void Printer_shutdown()
+{
+    pthread_join(thread, NULL);
 }
